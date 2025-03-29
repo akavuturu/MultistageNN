133bb547b4b8ab35495f282f1a4729f23ff6f564
akavuturu <akavuturu@gmail.com>
2025-03-29 17:19:29 -0400

    added cluster scripts to local for reference

---
+++ gpu_test.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
+print("GPUs available:", tf.config.list_physical_devices('GPU'))
+
+# # # Simple GPU computation test
+# with tf.device('GPU:0'):
+#     a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+#     b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+#     c = tf.matmul(a, b)
+# print("Matrix Multiplication Result:", c)
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  for gpu in gpus:
+    print("Name:", gpu.name, "Type:", gpu.device_type)
+else:
+  print("No GPUs found")
